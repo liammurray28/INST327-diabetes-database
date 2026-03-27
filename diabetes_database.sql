cef5f3075b629ceb85253774c5e0d355f5b0b226
lxsxd1217 <alin1217@terpmail.umd.edu>
2026-03-27 11:37:17 -0400

    added the rest of the table

--- diabetes_database.sql
+++ diabetes_database.sql
@@ -30,3 +30,56 @@ CREATE TABLE patients_diabetes (
     FOREIGN KEY (diabetes_id) REFERENCES diabetes(diabetes_id),
 
 );
+
+CREATE TABLE external_factors (
+
+    factor_id PRIMARY KEY AUTO_INCREMENT,
+    FOREIGN KEY (patient_id) REFERENCES patients(patient_id),
+    physical_activity VARCHAR(255),
+    dietary_habits VARCHAR(255),
+    socioeconomic_factors VARCHAR(255),
+    environmental_factors VARCHAR(255),
+    alcohol_consumption VARCHAR(255),
+    smoking_status VARCHAR(255) 
+
+)
+
+CREATE TABLE genetics (
+
+    genetic_id PRIMARY KEY AUTO_INCREMENT,
+    FOREIGN KEY (patient_id) REFERENCES patients(patient_id),
+    patient_race VARCHAR(255),
+    patient_ethnicity VARCHAR(255),
+    genetic_markers VARCHAR(255),
+    family_history VARCHAR(255),
+    cystic_fibrosis_diagnosis VARCHAR(255)
+
+)
+
+CREATE TABLE tests (
+
+    test_id PRIMARY KEY AUTO_INCREMENT,
+    FOREIGN KEY (patient_id) REFERENCES patients(patient_id),
+    blood_pressure INT,
+    blood_glucose_levels INT,
+    cholesterol_levels INT,
+    autoantibodies VARCHAR(255),
+    glucose_tolerance_test VARCHAR(255),
+    urine_test VARCHAR(255),
+    pulmonary_function INT,
+    pancreatic_health INT,
+    insulin_levels INT,
+    digestive_enzyme_levels	INT
+
+)
+
+CREATE TABLE reproduction (
+
+    reproductive_id PRIMARY KEY AUTO_INCREMENT,
+    FOREIGN KEY (patient_id) REFERENCES patients(patient_id),
+    history_of_pcos VARCHAR(255),
+    pregnancy history VARCHAR(255),
+    weight_gain_during_pregnancy INT,
+    previous_gestational_diabetes VARCHAR(255)
+
+)
